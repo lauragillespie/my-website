bded85d4e0e2d1e8fe4d75511c8d8b57f16f7445
Laura <legillespie5@hotmail.com>
2021-11-11 15:58:40 -0800

    file structure

---
+++ public/scripts/firebase.js
@@ -0,0 +1,19 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBCzW1aT0oRxrTV6lZTFXWhayJUjXMEhjY",
+  authDomain: "lauragillespie-14e3f.firebaseapp.com",
+  projectId: "lauragillespie-14e3f",
+  storageBucket: "lauragillespie-14e3f.appspot.com",
+  messagingSenderId: "674948983359",
+  appId: "1:674948983359:web:d2500eef2532ac5262e68a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
